5185fa27db803865dd4e398f8d09db5c9c8ef4b1
itachi1358 <arshadyasir123@gmail.com>
2025-02-03 15:33:14 +0530

    Removed FontAwesome

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Megaevents from './components/Megaevents';
 import PastEvents from './components/PastEvents';
 import Footer from './components/Footer';
 import Merchandise from './components/Merchandise';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home'
 import Team from './components/Team';
