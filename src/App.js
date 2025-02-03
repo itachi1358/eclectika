a4821e6baf8841d204fce622e4bb7b7cf0d21c5f
itachi1358 <arshadyasir123@gmail.com>
2025-02-03 17:05:00 +0530

    fixed the mega event card button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import About from './components/About_us';
-import OurTheme from './components/OurTheme';
-import Megaevents from './components/Megaevents';
-import PastEvents from './components/PastEvents';
-import Footer from './components/Footer';
+
 import Merchandise from './components/Merchandise';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home'
